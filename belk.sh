d301489265a7535c8d341102593e28fe6400f0f8
vector1one <39304498+vector1one@users.noreply.github.com>
2024-06-20 12:23:01 -0400

    Update and rename belk to belk.sh

--- belk.sh
+++ belk.sh
@@ -3,6 +3,7 @@
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 sudo apt-get install apt-transport-https   
+sudo apt-get update && sudo apt-get install elasticsearch
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 echo "action.auto_create_index: .monitoring*,.watches,.triggered_watches,.watcher-history*,.ml*" >> /etc/elasticsearch/elasticsearch.yml
 
