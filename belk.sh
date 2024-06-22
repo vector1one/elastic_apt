f9636c06cc781aa54765b5b8db5b81a0aca826ad
Jonesckevin <57996927+Jonesckevin@users.noreply.github.com>
2024-06-22 13:17:38 -0400

    Update belk.sh

--- belk.sh
+++ belk.sh
@@ -1,68 +1,16 @@
 #!/bin/bash
 
-
 HOSTIP=$(hostname -I | awk '{print $1}')
 
-## Prompt user to uninstall any existing ElasticSearch packages
-uninstallElasticsearch() {
-  # Stop Elasticsearch service
-  read -p "Do you want to uninstall any existing ElasticSearch packages? (y/N): " uninstallChoice
-
-  if [[ $uninstallChoice == "y" ]]; then
-    # Stop Elasticsearch service
-    sudo systemctl stop elasticsearch.service
-
-    # Disable Elasticsearch service
-    sudo systemctl disable elasticsearch.service
-
-    # Uninstall Elasticsearch package
-    sudo apt-get remove --purge elasticsearch
-
-    # Remove Elasticsearch data and logs
-    sudo rm -rf /var/lib/elasticsearch/
-    sudo rm -rf /var/log/elasticsearch/
-
-    # Remove Elasticsearch configuration files
-    sudo rm -rf /etc/elasticsearch/
-
-    # Update the package list
-    sudo apt-get update
-
-    # Reinstall Elasticsearch
-    # First, add the GPG key for the official Elasticsearch repository
-    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-
-    # Add the Elasticsearch repository
-    echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-
-    # Update the package list again
-    sudo apt-get update
-  fi
-}
-uninstallElasticsearch
-
-
-#### install basic elastic 
-installElasicsearch() {
-  # Install the apt-transport-https package
-  sudo apt-get install -y apt-transport-https
-
-  # Update the package list and install Elasticsearch
-  sudo apt-get update && sudo apt-get install elasticsearch -y
-
-## Download and import the GPG key for Elasticsearch
+# Install Basic Elastic 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 
-### Add the Elasticsearch repository to the sources list
+sudo apt-get install apt-transport-https   
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
-### Update the package list and install Elasticsearch
-sudo apt-get update && sudo apt-get install -y elasticsearch
-}
-installElasicsearch
+sudo apt-get update
 
-
-modifyElasticsearchConfig() {
+configElasticsearch() {
   ## Modify "/etc/elasticsearch/elasticsearch.yml" first before starting service
   #nano /etc/elasticsearch/elasticsearch.yml
 
@@ -77,7 +25,7 @@ modifyElasticsearchConfig() {
 
   ## User Prompt: Configure the network host in "/etc/elasticsearch/elasticsearch.yml"
   echo "network.host: $HOSTIP is set in /etc/elasticsearch/elasticsearch.yml"
-  sed -i "s/#network.host: 192.168.0.1/network.host: $(hostname -I | awk '{print $1}')/" /etc/elasticsearch/elasticsearch.yml
+  sed -i "s/#network.host: .*/network.host: $HOSTIP/" /etc/elasticsearch/elasticsearch.yml
   echo
 
   ## User Prompt: Uncomment http.port in "/etc/elasticsearch/elasticsearch.yml"
@@ -86,55 +34,94 @@ modifyElasticsearchConfig() {
   echo
 
   ## Start Elasticsearch
+  echo
   echo "Starting the ElasticSearch service..."
   systemctl daemon-reload
   systemctl enable elasticsearch.service
   systemctl start elasticsearch
   echo "ElasticSearch service has been started."
+  echo
 }
-modifyElasticsearchConfig
 
-## Once edited, obtain the enrollment token from the master node
-node1token=$(/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node)
-# E.g.: eyJ2ZXIiOiI4LjE0LjAiLCJhZHIiOlsiMTAuMS4zLjE1MDo5MjAwIl0sImZnciI6IjMxODUwZDk3MzFlMTEyNzBiNTkwMjYyNDJmYTNjMWUyN2U0MWJlODMzNzYwYzg5NmNhNjhkODIyMmFiMDliNWQiLCJrZXkiOiJac1NnUHBBQjk1bWRscEVKSmFFXzo3cmp2QldBN1M1NlRwRHJackhHcmZBIn0=
-echo "~~~~~~~~~~~~~~~"
-echo " The enrollment token for the joining node is: "
-echo " $node1token "
-echo "~~~~~~~~~~~~~~~"
-echo
+configKibana() {
+  # Edit config for Splash Page for /etc/kibana/kibana.yml
+  echo "Configuring Kibana..."
+  echo "Creating Kibana Token..."
+  
+  echo "network.host: 0.0.0.0 is set in /etc/kibana/kibana.yml"
 
-## Paste the enrollment token into the joining node
-echo "-----------------------------------------------------------------"
-echo "Now take the following command and run it on the joining node after setting up the secondary nodes"
-echo
-echo "/usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token $node1token"
-echo "-----------------------------------------------------------------"
-### ~~~ ***Generate a new token for each node*** ~~~ ###
-echo
+  sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/g' /etc/kibana/kibana.yml | tr -d '\r'
+  echo "Starting Kibana..."
+  systemctl enable kibana
+  systemctl start kibana
 
-## User Prompt: Check the status of the ElasticSearch service or do a Test Curl
-while true; do
-  read -p "Do you want to check the status of the ElasticSearch service? 
-  [1] - Check the status of the ElasticSearch service
-  [2] - Test Curl to http://$HOSTIP:9200
-  Choice: " choice
-  case $choice in
-    1 )
-      systemctl status elasticsearch
-      ;;
-    2 )
-      curl http://$HOSTIP:9200
-      ;;
-    [Nn]* )
-      break
-      ;;
-    [Qq]* )
-      exit
-      ;;
-    * )
-      echo "Please answer 1, 2, q."
-      ;;
-  esac
-done
+  echo "Waiting 30 seconds for Kibana to start..."
+  sleep 30
 
-#profit
+  echo "Resetting password for the built-in superuser 'elastic'..."
+  /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic
+
+  echo "Creating backup user 'admin' with password 'Cyber!23'..."
+  /usr/share/elasticsearch/bin/elasticsearch-users useradd admin -p Cyber\!23 -r superuser
+
+  #Create Kibana Token
+  kibanatoken=$(/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana)
+  echo "Kibana token created"
+  echo "~~~~~~~~~~~~~~~"
+  echo " The enrollment token for the joining node is: "
+  echo " $kibanatoken "
+  echo "~~~~~~~~~~~~~~~"
+  echo "Navigate to http://$HOSTIP:5601 and paste in the token to login."
+
+  echo && read -n 1 -s -r -p "Press any key to continue" && echo
+
+
+  kibanaotp=$(/usr/share/kibana/bin/kibana-verification-code)
+  echo "Kibana OTP created"
+  echo "~~~~~~~~~~~~~~~"
+  echo " The enrollment token for the joining node is: "
+  echo " $kibanaotp "
+  echo "~~~~~~~~~~~~~~~"
+
+  echo && read -n 1 -s -r -p "Press any key to continue" && echo
+}
+
+# Elastic Token Join for Non-Master Nodes
+elasticTokenJoin() {
+  if [[ $is_master == "y" ]]; then
+    echo
+    echo "This is the master node. So you should be getting the Elastic Node Joining Token."
+    elasticenroll=$(/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node)
+    echo "Elastic token created"
+    echo "~~~~~~~~~~~~~~~"
+    echo " The enrollment token for the joining node is: "
+    echo " /usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token $elasticenroll "
+    echo "~~~~~~~~~~~~~~~"
+  else
+    echo
+    echo "ReIterating this is not the master node. Take your master Joining token ennrollment and do this now."
+    read -p "Enter the Elastic Node Joining Token Only: " elasticToken
+    echo && echo "/usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token $elasticToken"
+    /usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token $elasticToken
+
+  fi
+  echo && read -n 1 -s -r -p "Press any key to continue" && echo
+}
+
+# Master Node & Install Kibana
+read -p "Is this the master node? (y/n): " is_master
+if [[ $is_master == "y" ]]; then
+  sudo apt install kibana
+  sudo apt-get install elasticsearch
+  configElasticsearch  
+  configKibana
+  elasticTokenJoin
+else
+  echo
+  echo "This is not the master node. Skipping Kibana installation."
+  sudo apt-get install elasticsearch
+  configElasticsearch
+  elasticTokenJoin
+fi
+
+echo && echo "profit"
