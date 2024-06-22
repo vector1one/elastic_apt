7c1bf9a22490bfc8d60d72faf346b177d98d7a5a
vector1one <39304498+vector1one@users.noreply.github.com>
2024-06-22 02:03:37 -0400

    Update belk.sh

--- belk.sh
+++ belk.sh
@@ -11,14 +11,15 @@ sudo apt-get update && sudo apt-get install elasticsearch
 #edit "/etc/elasticsearch/elasticsearch.yml" first before starting service
 
   #cluster.name: my-application
-  #node.name: node-1
+  #node.name: node-1 (change for each node ex node-2, node-3...etc)
   #network.host: 10.3.10.155 ( ip addr of node)
   #http.port: 9200
 
 #once edited get token from master 
   #/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node
+  #***generate new token for each node****
 
-#paste into joining node
+#copy and paste into joining node
   #/usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token <enrollment-token>
 #***generate new token for each node****
 
